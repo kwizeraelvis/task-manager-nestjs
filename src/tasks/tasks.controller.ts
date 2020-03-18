c0a7583f297fd6415d0bb0bfcb6b1400c71edf34
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-18 19:14:38 +0200

    Implement get task by id functionality

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Request, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,8 +12,14 @@ export class TasksController {
       return this.taskService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string){
+        return this.taskService.getTaskById(id);
+    }
+
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task{
         return this.taskService.createTask(createTaskDto);
     }
+
 }
